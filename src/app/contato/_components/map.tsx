bd5f86f732d9027d84068ecede5d2a5939f9fd1a
pedro <pedrof.andrade16@gmail.com>
2024-11-18 16:19:13 -0300

    update map

--- src/app/contato/_components/map.tsx
+++ src/app/contato/_components/map.tsx
@@ -4,7 +4,7 @@ export function Map() {
       <iframe
         width="1280"
         height="600"
-        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Rua%20Maria%20da%20Piedade%20Moreira,%20n%C2%BA%201000%20%E2%80%93%20Bairro%20-%20C%C3%B3rrego%20Frio,%20Santa%20Luzia%20-%20MG,%2033026-000+(CRISGUI)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
       />
     </div>
   );
