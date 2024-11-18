5ccb518e69a0a1781b452b7419ef1954484c8728
pedro <pedrof.andrade16@gmail.com>
2024-11-18 16:19:51 -0300

    update map

--- src/app/orcamento/_components/map.tsx
+++ src/app/orcamento/_components/map.tsx
@@ -3,7 +3,7 @@ export function Map() {
     <div className="max-w-screen-xl sm:w-full mx-4 sm:mx-auto mt-14 sm:mt-32">
       <iframe
         className="w-full lg:w-full h-96"
-        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Rua%20Maria%20da%20Piedade%20Moreira,%20n%C2%BA%201000%20%E2%80%93%20Bairro%20-%20C%C3%B3rrego%20Frio,%20Santa%20Luzia%20-%20MG,%2033026-000+(CRISGUI)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
       />
     </div>
   );
